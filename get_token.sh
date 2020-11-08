17ce4fa540f59755e4df358a73a445433edb6bae
Chris Clark <chris.clark@globalstripe.com>
2020-11-08 00:15:13 +0000

    Added Get Token Code

---
+++ get_token.sh
@@ -0,0 +1,4 @@
+curl --location --request POST 'http://localhost:8000/api/token/' \
+--header 'Authorization: Token 074e044e42e409a3434ddc65895d114b3c87ebe1' \
+--form 'username=cclark' \
+--form 'password=Adv@18241'
